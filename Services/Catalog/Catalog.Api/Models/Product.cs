232763d937d4a2c8d878a80751d265edd9f22949
David Israel <davidmorenoresendezisrael@gmail.com>
2024-11-10 15:59:37 -0600

    Catalogs API
    Catalogs service

---
+++ Services/Catalog/Catalog.Api/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Catalog.Api.Models;
+
+public class Product
+{
+    public Guid id { get; set; }
+    public string Name { get; set; } = default!;
+    public List<string> Categories { get; set; } = new();
+    public string Description { get; set; } = default!;
+    public string ImageFile { get; set; } = default!;
+    public double Price { get; set; }
+}
---
+++ Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,32 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.Api.Models;
+
+namespace Catalog.Api.Products.CreateProduct;
+
+public record CreateProductCommand(
+    string Name,
+    List<string> Categories,
+    string Description,
+    string ImageFile,
+    double Price
+) : ICommand<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        Product product = new()
+        {
+            id = Guid.NewGuid(),
+            Name = command.Name,
+            Categories = command.Categories,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+
+        return new CreateProductResult(product.id);
+    }
+}
